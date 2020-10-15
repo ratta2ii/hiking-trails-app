6d8119bf70ca92a6b852c95e70d43635f93d2d58
Trevor Mackin <tismetrm@gmail.com>
2020-10-14 23:58:28 -0700

    Wrap the application in both a HashRouter, as well as the redux store Provider tags impoort middlewareLogger and thunkMiddleware that will be utilized in asynchronous actions later in the project Create a store with the built-in redux method, while also passing in both the rootReducer and middleware as arguments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import Home from './Home';
+import Trails from './Trails/Trails';
+import Footer from './Components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props) {
+
+    return (
+        <Fragment>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path="/trails" component={Trails} />
+            </Switch>
+            <Footer />
+        </Fragment>
+    );
 }
 
-export default App;
+export default withRouter(App);
