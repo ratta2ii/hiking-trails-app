6b4d77e4fb3168aae643655d5b401407be082bd5
Trevor Mackin <tismetrm@gmail.com>
2020-10-18 23:40:34 -0700

    Convert the Home class component into a functional component

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 import { getGeoLocation } from './actions/locationActions';
+import { makeApiCall } from './actions/trailsActions';
+// import { Link } from "react-router-dom";
 
 
-class Home extends React.Component {
+const Home = (props) => {
 
 
-    componentDidMount() {
-        const { dispatch } = this.props;
+    const { dispatch, coords, coordsError } = props;
+
+
+    useEffect(() => {
         console.log("inside Home mounting");
         dispatch(getGeoLocation());
+    }, []);
+    
+
+    const handleFindTrails = () => {
+        dispatch(makeApiCall());
+        props.history.push('/trails');
     }
 
 
-    render() {
-
-        const { coords, coordsError } = this.props;
-
-        if (coordsError) {
-            return <React.Fragment>Error: {coordsError.message}</React.Fragment>;
-        } else {
-            return (
-                <React.Fragment>
-                    <h3>Latittude: {coords.latitude}</h3>
-                    <h3>Longitude: {coords.longitude}</h3>
-                    <Link to="/trails">Find Trails</Link>
-                </React.Fragment>
-            );
-        }
+    if (coordsError) {
+        return <React.Fragment>Error: {coordsError.message}</React.Fragment>;
+    } 
+    else {
+        return (
+            <React.Fragment>
+                <h3>Latittude: {coords.latitude}</h3>
+                <h3>Longitude: {coords.longitude}</h3>
+                <Button onClick={handleFindTrails} >Find Trails</Button>
+                {/* <Link to="/trails">Find Trails</Link> */}
+            </React.Fragment>
+        );
     }
 }
 
@@ -42,3 +49,50 @@ const mapStateToProps = state => {
 
 
 export default connect(mapStateToProps)(Home);
+
+
+
+
+
+
+
+
+//! Same component as a functional component
+// class Home extends React.Component {
+
+
+//     componentDidMount() {
+//         const { dispatch } = this.props;
+//         console.log("inside Home mounting");
+//         dispatch(getGeoLocation());
+//     }
+
+
+//     render() {
+
+//         const { coords, coordsError } = this.props;
+
+//         if (coordsError) {
+//             return <React.Fragment>Error: {coordsError.message}</React.Fragment>;
+//         } else {
+//             return (
+//                 <React.Fragment>
+//                     <h3>Latittude: {coords.latitude}</h3>
+//                     <h3>Longitude: {coords.longitude}</h3>
+//                     <Link to="/trails">Find Trails</Link>
+//                 </React.Fragment>
+//             );
+//         }
+//     }
+// }
+
+
+// const mapStateToProps = state => {
+//     return {
+//         coords: state.locationSlice.coords,
+//         coordsError: state.locationSlice.error,
+//     }
+// }
+
+
+// export default connect(mapStateToProps)(Home);
